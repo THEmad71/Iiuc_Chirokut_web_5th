2697b1ddbadc87fca8d01dc17ad3e616f50f91da
THEmad71 <mrbangladesh19521971@gmail.com>
2024-12-21 04:20:45 +0600

    pain

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import SignIn from './SignIn';
+import SignUp from './SignUp'; // Import the SignUp component
+import StudentPage from './StudentPage';
+import Home from './Home';
 
-function App() {
+const App = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/signin"
+          element={<SignIn setIsAuthenticated={setIsAuthenticated} />}
+        />
+        <Route path="/signup" element={<SignUp />} />
+        <Route
+          path="/student"
+          element={isAuthenticated ? <StudentPage /> : <SignIn setIsAuthenticated={setIsAuthenticated} />}
+        />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
